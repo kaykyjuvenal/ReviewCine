155a9133f9d0021e97051d86d55e858161ca353b
kaykyjuvenal <neguito.juvenal@gmail.com>
2025-04-22 21:20:08 -0300

    feat: Init Elenco and series java classes

--- src/main/java/org/br/edu/ifsp/reviewcine/Elenco.java
+++ src/main/java/org/br/edu/ifsp/reviewcine/Elenco.java
@@ -1,12 +1,12 @@
 package org.br.edu.ifsp.reviewcine;
 
+import java.util.List;
 import java.util.Optional;
 
 public class Elenco {
-    public class Elenco {
-        private int id;
-        private Filmes filmes;
-        Optional<Series> series;
-        private List<Pessoas> pessoas;
-    }
+
+    private int id;
+    Optional<Integer> idFilme;
+    Optional<Integer> idSerie;
+    private List<Pessoa> pessoas;
 }
---
+++ src/main/java/org/br/edu/ifsp/reviewcine/Pessoa.java
@@ -0,0 +1,12 @@
+package org.br.edu.ifsp.reviewcine;
+
+public class Pessoa {
+    private int id;
+    private String character;
+    private String profile_path;
+    private String gender;
+    private String artist_name;
+    private String department;
+    private boolean adult;
+    private double popularity;
+}
