a64b41cc277b4fa008e14b473353954d1824d072
kaykyjuvenal <neguito.juvenal@gmail.com>
2025-04-22 21:14:18 -0300

    feat: Init Elenco and series java classes

---
+++ src/main/java/org/br/edu/ifsp/reviewcine/Elenco.java
@@ -0,0 +1,12 @@
+package org.br.edu.ifsp.reviewcine;
+
+import java.util.Optional;
+
+public class Elenco {
+    public class Elenco {
+        private int id;
+        private Filmes filmes;
+        Optional<Series> series;
+        private List<Pessoas> pessoas;
+    }
+}
